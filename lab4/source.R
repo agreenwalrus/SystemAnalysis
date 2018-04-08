fb1b0d0f9c2de6aa5e225f25a0fa9e56302ccb72
Julia Runova <jul.runova@gmail.com>
2018-04-08 19:21:45 +0300

    analize new text and count its coordinates

--- lab4/source.R
+++ lab4/source.R
@@ -11,12 +11,14 @@ prepareTextForAnalizing <- function(text) {
   result <- tm_map(result, removeNumbers)
   result <- tm_map(result, removeWords, stopwords("english"))
   result <- tm_map(result, removePunctuation)
+  
   return(result)
 }
 
 sortWordsByFreq <- function(dict) {
   v <- sort(rowSums(dict),decreasing=TRUE)
   sortedDict <- data.frame(word = names(v),freq=v)
+  
   return(sortedDict)
 }
 
@@ -24,10 +26,11 @@ formDictionary <- function(preparedText) {
   docs <- tm_map(preparedText, stemDocument)
   dtm <- TermDocumentMatrix(docs)
   dict <- as.matrix(dtm)
+  
   return(dict)
 } 
 
-getMostPopularWords <- function(dirPath, amountOfWords){
+getDictionary <- function(dirPath, amountOfWords = -1){
   articles <- Corpus(DirSource(dirPath),
                      readerControl = list(reader = readPlain,
                                           language = "en",
@@ -35,7 +38,12 @@ getMostPopularWords <- function(dirPath, amountOfWords){
   
   articles <- prepareTextForAnalizing(articles)
   dict <- formDictionary(articles)
-  h <- head(sortWordsByFreq(dict), amountOfWords)
+  h <- sortWordsByFreq(dict)
+  
+  if(amountOfWords != -1) {
+    h <- head(h, amountOfWords)
+  }
+  
   return(h)
 }
 
@@ -47,11 +55,42 @@ getMapFromDictionary <- function(dict) {
   return(map)
 }
 
+getCoordinatesOfDict <- function(vectors, mappedDict){
+  result <- c(0, 0, 0)
+  keys <- names(mappedDict)
+  print(result)
+  print(vectors)
+  print(length(vectors))
+  
+  for (key in keys) {
+    for(i in 1:length(vectors)) {
+      if (key %in% names(vectors[[i]])){
+        result[i] <- result[i] + mappedDict[key]
+      }
+    }
+  }
+  
+  return(result)
+}
 
-# Файлы хранятся в каталоге articles
-dirpath <- "C:\\lab4\\SA_texts\\teach"
-h <- getMostPopularWords(dirpath, 20)
-foo <- getMapFromDictionary(h)
+dirPaths <- c("C:\\SA_texts\\lab4\\teach\\crypto", 
+              "C:\\SA_texts\\lab4\\teach\\space", 
+              "C:\\SA_texts\\lab4\\teach\\cloning")
 
-foo
+testDir <- "C:\\SA_texts\\lab4\\test\\test"
+
+dicts <- list()
+i <- 1
+for(path in dirPaths) {
+  h <- getDictionary(path, 60)
+  dicts[[i]] <- getMapFromDictionary(h)
+  i <- i + 1
+}
+
+h <- getDictionary(testDir)
+testDict <- getMapFromDictionary(h)
+
+a <- getCoordinatesOfDict(vectors = dicts, mappedDict = testDict)
+a
+
 
