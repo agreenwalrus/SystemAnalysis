fe41613169671e3b516f0c74e23e6f45b364175d
Julia Runova <jul.runova@gmail.com>
2018-04-16 13:53:24 +0300

    good plots

--- lab4/source.R
+++ lab4/source.R
@@ -1,8 +1,9 @@
 library(tm)
 library("SnowballC")
+library(rgl)
 #install.packages("tm", dependencies = T)
 #install.packages("SnowballC", dependencies = T)
-
+#install.packages("rgl")
 
 prepareTextForAnalizing <- function(text) {
   result <- tm_map(text, iconv, 'cp1251', 'UTF-8')
@@ -101,20 +102,40 @@ getCoordinatesOfDict <- function(vectors, mappedDict){
 library(scatterplot3d) 
 attach(mtcars)
 
+addPoints <- function(coord, toClust){
+  #newdf <- data.frame(data, K=clustVec)#Include the number of cluster in the data set
+  #pcdf <- princomp(data)#,cor=T,score=T)
+  #summary(pcdf)#Compute the validity of each component/dimension
+  plot3d(x = coord[1],
+         y = coord[2],
+         z = coord[3],
+         col=toClust, 
+         add = T)#Create a 3D plot
+}
+
+clusterPlot3D <- function(data, kmRes){
+  newdf <- data.frame(data, K=kmRes$cluster)#Include the number of cluster in the data set
+  pcdf <- princomp(data)#,cor=T,score=T)
+  summary(pcdf)#Compute the validity of each component/dimension
+  plot3d(pcdf$scores, 
+         col=newdf$K, 
+         xlab = "crypto", 
+         ylab = "space", 
+         zlab = "cloning")#Create a 3D plot
+}
+
 build3dPlot <- function(data) {
   
   crypto <- unlist(lapply(data, function(x) x[[2]][1]))
   space <- unlist(lapply(data, function(x) x[[2]][2]))
   cloning <- unlist(lapply(data, function(x) x[[2]][3]))
-  print(crypto)
-  print(space)
-  print(cloning)
   
   scatterplot3d(crypto, space, cloning, main="Text plot")
 }
 
 getListOfFilesCoordinates <- function(filesPath, dicts) {
   i <- 1
+  testFilesCoordinates <- list()
   for(fileName in filesPath) {
     h <- getDictionary(getCorpusSingleFile(fileName))
     testDict <- getMapFromDictionary(h)
@@ -138,7 +159,37 @@ getListOfDictsForTexts <- function(dictPaths, sizeOfDict) {
   return(dicts)
 }
 
+getPrediction <- function(coord, clusters) {
+  
+  predict <- c(sqrt((clusters[1,][1] - coord[1])**2 + 
+                     (clusters[1,][2] - coord[2])**2 +
+                     (clusters[1,][3] - coord[3])**2), 1)
+  len <- 1
+  clustNum <- 2
+  
+  i <- 1
+  for(i in 2:3) {
+  
+    current <- sqrt((clusters[i,][1] - coord[1])**2 + 
+                      (clusters[i,][2] - coord[2])**2 +
+                      (clusters[i,][3] - coord[3])**2)
 
+    #print(predict)
+    #print(i)
+    #print(current)
+    if(current < predict[len]){
+      predict <- c(current, i)
+    }
+    
+    i <- i + 1
+  }
+  
+  return(predict)
+}
+
+
+
+#######################################
 dictPaths <- c("C:\\SA_texts\\lab4\\dicts\\crypto", 
               "C:\\SA_texts\\lab4\\dicts\\space", 
               "C:\\SA_texts\\lab4\\dicts\\cloning")
@@ -151,10 +202,10 @@ testFiles <- c("cloning4.txt", "cloning5.txt",
 kMeanFiles <- c("cloning1.txt", "cloning2.txt", "cloning3.txt", 
                "space1.txt", "space2.txt", "space3.txt",
                "crypto1.txt", "crypto2.txt", "crypto3.txt")
+######################################
 
 
-
-#dicts
+#form files' names
 kMeanFiles <- unlist(lapply(kMeanFiles, function(fname, dir){
   paste(dir, fname, sep="")
 }, dir = filesDir))
@@ -163,30 +214,71 @@ testFiles <- unlist(lapply(testFiles, function(fname, dir){
   paste(dir, fname, sep="")
 }, dir = filesDir))
 
-kMeanFiles <- c (kMeanFiles, testFiles)
 
 
+#form dictionary
 dicts <- getListOfDictsForTexts(dictPaths = dictPaths, sizeOfDict = 60)
 
-fc <- getListOfFilesCoordinates(filesPath = kMeanFiles, 
+#form coordinates of files according to dictionaries
+#for k-meanes files
+kmfc <- getListOfFilesCoordinates(filesPath = kMeanFiles, 
                                dicts = dicts)
 
-build3dPlot(fc)
+build3dPlot(kmfc)
 
-fc
+#form coordinates of files according to dictionaries
+#for test files
+tfc <- getListOfFilesCoordinates(filesPath = testFiles, 
+                                 dicts = dicts)
 
-
-l <- lapply(d, function(el) el[[2]])
+#form matrix of coordinates
+l <- lapply(kmfc, function(el) el[[2]])
 m <- matrix(unlist(l), ncol = 3, byrow = TRUE)
 
-res <- kmeans(x = m, centers = 3)
+
+amountOfClusters <- 3
+res <- kmeans(x = m, centers = amountOfClusters)
+print("K-MEANS RESULT")
 res
 
-clasters <- matrix(res$centers, ncol=3)
+clusterVector <- res$cluster
+for(i in 1:amountOfClusters) {
+  clNum <- 1
+  print(paste("CLUSTER GROUP ¹", toString(i), sep=""))
+  print(paste("Centers:", toString(res$centers[i,], sep = "")))
+  for(cl in clusterVector){
+    if(cl == i){
+      print(kMeanFiles[clNum])
+    }
+      clNum <- clNum + 1
+  }
+}
+  
+clusters <- matrix(res$centers, ncol=3)
+
+print("-----------------------------------------------")
+
+clusterPlot3D(data = m, kmRes = res)
+
+
+print("CLUSTERS")
+
+for(fil in tfc){
+  pred <- getPrediction(coord = fil[[2]], clusters = clusters)
+  print(paste(fil[[1]], "to group ¹", pred[2], sep=" "))
+  addPoints(fil[[2]], pred[2])
+  print("")        
+}
+
+
+print("-----------------------------------------------")
 
 
 
-res$centers[[1]]
+
+
+
+
 
 
 
